8b1a04715fcbf83f40fa84a39ed54cc8c3e07df9
Rory Graves <rory.graves@fieldmark.co.uk>
2018-12-16 20:53:32 +0000

    First version

---
+++ input/src/main/scala/fix/Scalaclean.scala
@@ -0,0 +1,26 @@
+/*
+rule = Scalaclean
+*/
+package fix
+
+object TestMain {
+  def main(args: Array[String]) = {
+
+    val used = UsedClass(5)
+    println(used.usedMethod(true, 3))
+  }
+
+}
+
+case class UsedClass(value: Int)  {
+  def usedMethod(myBool: Boolean, y: Int) : Int = {
+    y + value
+  }
+
+  def unusedMethod(z: Int): String = {
+    ???
+  }
+
+}
+
+case class UnusedClass(value: String)
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Scalafix rules for ScalaClean
+
+To develop rule:
+```
+sbt ~tests/test
+# edit rules/src/main/scala/fix/Scalaclean.scala
+```
