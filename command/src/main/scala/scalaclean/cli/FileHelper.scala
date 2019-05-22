04586b5ab5f4d25859ceaebc5bc3e7dabfd8d117
Mike Skells <mike.skells@talk21.com>
2019-05-22 22:59:08 +0100

    FileHelper

---
+++ command/src/main/scala/scalaclean/cli/FileHelper.scala
@@ -0,0 +1,20 @@
+package scalaclean.cli
+
+object FileHelper {
+
+  def home = System.getProperty("user.home")
+  def fileSep = System.getProperty("file.separator")
+  def pathSep = System.getProperty("path.separator")
+
+
+  def toPlatform(fileOrPath: String) = {
+    var res = fileOrPath
+
+    res = res.replace("/", fileSep)
+    res = res.replace(":", pathSep)
+    res = res.replace("$HOME$", home)
+
+    res
+
+  }
+}
