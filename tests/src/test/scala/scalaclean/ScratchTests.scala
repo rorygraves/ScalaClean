78cf0e2bf05f410833b79fd7651501967feba78d
Dale Wijnand <dale.wijnand@gmail.com>
2020-03-10 09:04:56 +0000

    Try to mitigate missing sources in ScratchTests
    "Try" because catching NoSuchFileException is an approximation.

--- tests/src/test/scala/scalaclean/ScratchTests.scala
+++ tests/src/test/scala/scalaclean/ScratchTests.scala
@@ -1,9 +1,14 @@
 package scalaclean
 
-class ScratchTests extends  AbstractProjectTests {
+import java.nio.file.NoSuchFileException
 
+import org.scalatest.exceptions.TestPendingException
+
+class ScratchTests extends AbstractProjectTests {
   test("scratch") {
-    deadCodeProjectTest("scratch")
+    try deadCodeProjectTest("scratch") catch {
+      case e: NoSuchFileException => throw new TestPendingException().initCause(e)
+    }
   }
 
 //  test("scratch1") {
