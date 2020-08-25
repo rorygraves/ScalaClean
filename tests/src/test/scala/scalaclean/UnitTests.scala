1afe9ba7d27aa03396d1c2de4ece3f8991159005
Rory Graves <rory.graves@fieldmark.co.uk>
2020-08-25 13:56:17 +0100

    Update test expectations

--- tests/src/test/scala/scalaclean/UnitTests.scala
+++ tests/src/test/scala/scalaclean/UnitTests.scala
@@ -111,11 +111,52 @@ trait AbstractUnitTests extends AnyFunSuite with AssertionsForJUnit with DiffAss
 
 class UnitTests extends AbstractUnitTests {
 
+  test("nodesTest") {
+    runTest("scalaclean/test/nodes/nodes.scala", new TestNodes(_))
+  }
+
   test("akkaTimeoutTest") {
     runTest("scalaclean/test/akka/Timeout.scala", new Test_allTransitiveOverrides(_))
   }
 
   test("internalOutgoingReferences") {
     runTest("scalaclean/test/references/internalOutgoingReferences/internalOutgoingReferences.scala", new Test_internalOutgoingReferences(_))
+  }
+
+  test("internalTransitiveOverriddenByTest") {
+    runTest("scalaclean/test/overriddenBy/internalTransitiveOverriddenBy/internalTransitiveOverriddenBy.scala", new Test_internalTransitiveOverriddenBy(_))
+  }
+
+  test("internalDirectOverriddenBy") {
+    runTest("scalaclean/test/overriddenBy/internalDirectOverriddenBy/internalDirectOverriddenBy.scala", new Test_internalTransitiveOverriddenBy(_))
+  }
+
+  test("allDirectOverrides") {
+    runTest("scalaclean/test/overrides/allDirectOverrides/allDirectOverrides.scala", new Test_allDirectOverrides(_))
+  }
+
+  test("allTransitiveOverrides") {
+    runTest("scalaclean/test/overrides/allTransitiveOverrides/allTransitiveOverrides.scala", new Test_allTransitiveOverrides(_))
+  }
+
+  test("internalDirectOverrides") {
+    runTest("scalaclean/test/overrides/internalDirectOverrides/internalDirectOverrides.scala", new Test_internalDirectOverrides(_))
+  }
+
+  test("internalTransitiveOverrides") {
+    runTest("scalaclean/test/overrides/internalTransitiveOverrides/internalTransitiveOverrides.scala", new Test_internalTransitiveOverrides(_))
+  }
+
+
+  test("allOutgoingReferences") {
+    runTest("scalaclean/test/references/allOutgoingReferences/allOutgoingReferences.scala", new Test_allOutgoingReferences(_))
+  }
+
+  test("internalIncomingReferences") {
+    runTest("scalaclean/test/references/internalIncomingReferences/internalIncomingReferences.scala", new Test_internalIncomingReferences(_))
+  }
+
+  test("annotations") {
+    runTest("scalaclean/test/annotation/Annotated.scala",new TestExtensions(_))
   }
 }
