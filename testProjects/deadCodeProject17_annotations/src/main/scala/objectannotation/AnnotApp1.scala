b646f6dba39fa0ed9e35f7e895d6a492cbe48ff8
Rory Graves <rory.graves@fieldmark.co.uk>
2020-10-30 14:19:13 +0000

    Update annotation test cases for annotation on method param

--- testProjects/deadCodeProject17_annotations/src/main/scala/objectannotation/AnnotApp1.scala
+++ testProjects/deadCodeProject17_annotations/src/main/scala/objectannotation/AnnotApp1.scala
@@ -2,6 +2,12 @@ package objectannotation
 
 object AnnotApp1 extends App {
   UsedObject
+
+  myMethod(3)
+
+  def myMethod(@UsedMethodAnnotation x: Int) = {
+
+  }
 }
 
 
@@ -15,3 +21,5 @@ class UsedAnnotation1(value: Class[_])
 class UsedByAnnotation1(value: Class[_])
 
 class UnusedAnnotation(value: Class[_])
+
+class UsedMethodAnnotation
